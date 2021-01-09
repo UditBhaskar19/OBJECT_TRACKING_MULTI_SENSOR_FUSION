feb776012bfdfdfb8a4cf7d3419cc5de6b3c864c
UditBhaskar91 <51980538+UditBhaskar91@users.noreply.github.com>
2021-01-09 07:58:32 +0530

    Global Data Scripts

---
+++ Scripts_Global_Variables_and_Sim_Data/SensorFusion_Script2_RESTRUCTURE_SIMULATION_DATA.m
@@ -0,0 +1,141 @@
+% ==============================================================================================================================================================
+% Author Name : Udit Bhaskar Talukdar
+% Date        : 05.09.2020
+% Description : This script rearranges the simulated sensor data such that the data can be accessed conveniently.
+%             : The simulated data is generated using matlab Automated Driving Toolbox
+% ==============================================================================================================================================================
+% EGO SENSOR ( Simulation Data )
+EGO_Sensor_Simulated_Data.px = EGO_PX;
+EGO_Sensor_Simulated_Data.py = EGO_PX;
+EGO_Sensor_Simulated_Data.vx = EGO_VX;
+EGO_Sensor_Simulated_Data.vy = EGO_VY;
+EGO_Sensor_Simulated_Data.yaw = EGO_YAW;
+EGO_Sensor_Simulated_Data.yawRate = EGO_YAWRATE;
+EGO_Sensor_Simulated_Data.detTimeStamp = EGO_TimeStamp;
+% ==============================================================================================================================================================
+% RADAR 1 ( Simulation Data )
+RAD1_Sensor_Simulated_Data.px = RAD1_PX; 
+RAD1_Sensor_Simulated_Data.py = RAD1_PY; 
+RAD1_Sensor_Simulated_Data.vx = RAD1_VX;
+RAD1_Sensor_Simulated_Data.vy = RAD1_VY;
+RAD1_Sensor_Simulated_Data.MeasNoiseCov = RAD1_MeasNoise; 
+RAD1_Sensor_Simulated_Data.snr = RAD1_SNR;
+RAD1_Sensor_Simulated_Data.detTimeStamp = RAD1_detTimeStamp;
+% ==============================================================================================================================================================
+% RADAR 2 ( Simulation Data )
+RAD2_Sensor_Simulated_Data.px = RAD2_PX; 
+RAD2_Sensor_Simulated_Data.py = RAD2_PY; 
+RAD2_Sensor_Simulated_Data.vx = RAD2_VX;
+RAD2_Sensor_Simulated_Data.vy = RAD2_VY;
+RAD2_Sensor_Simulated_Data.MeasNoiseCov = RAD2_MeasNoise; 
+RAD2_Sensor_Simulated_Data.snr = RAD2_SNR;
+RAD2_Sensor_Simulated_Data.detTimeStamp = RAD2_detTimeStamp;
+% ==============================================================================================================================================================
+% RADAR 3 ( Simulation Data )
+RAD3_Sensor_Simulated_Data.px = RAD3_PX; 
+RAD3_Sensor_Simulated_Data.py = RAD3_PY; 
+RAD3_Sensor_Simulated_Data.vx = RAD3_VX;
+RAD3_Sensor_Simulated_Data.vy = RAD3_VY;
+RAD3_Sensor_Simulated_Data.MeasNoiseCov = RAD3_MeasNoise; 
+RAD3_Sensor_Simulated_Data.snr = RAD3_SNR;
+RAD3_Sensor_Simulated_Data.detTimeStamp = RAD3_detTimeStamp;
+% ==============================================================================================================================================================
+% RADAR 4 ( Simulation Data )
+RAD4_Sensor_Simulated_Data.px = RAD4_PX; 
+RAD4_Sensor_Simulated_Data.py = RAD4_PY; 
+RAD4_Sensor_Simulated_Data.vx = RAD4_VX;
+RAD4_Sensor_Simulated_Data.vy = RAD4_VY;
+RAD4_Sensor_Simulated_Data.MeasNoiseCov = RAD4_MeasNoise; 
+RAD4_Sensor_Simulated_Data.snr = RAD4_SNR;
+RAD4_Sensor_Simulated_Data.detTimeStamp = RAD4_detTimeStamp;
+% ==============================================================================================================================================================
+% RADAR 5 ( Simulation Data )
+RAD5_Sensor_Simulated_Data.px = RAD5_PX; 
+RAD5_Sensor_Simulated_Data.py = RAD5_PY; 
+RAD5_Sensor_Simulated_Data.vx = RAD5_VX;
+RAD5_Sensor_Simulated_Data.vy = RAD5_VY;
+RAD5_Sensor_Simulated_Data.MeasNoiseCov = RAD5_MeasNoise; 
+RAD5_Sensor_Simulated_Data.snr = RAD5_SNR;
+RAD5_Sensor_Simulated_Data.detTimeStamp = RAD5_detTimeStamp;
+% ==============================================================================================================================================================
+% RADAR 6 ( Simulation Data )
+RAD6_Sensor_Simulated_Data.px = RAD6_PX; 
+RAD6_Sensor_Simulated_Data.py = RAD6_PY; 
+RAD6_Sensor_Simulated_Data.vx = RAD6_VX;
+RAD6_Sensor_Simulated_Data.vy = RAD6_VY;
+RAD6_Sensor_Simulated_Data.MeasNoiseCov = RAD6_MeasNoise; 
+RAD6_Sensor_Simulated_Data.snr = RAD6_SNR;
+RAD6_Sensor_Simulated_Data.detTimeStamp = RAD6_detTimeStamp;
+% ==============================================================================================================================================================
+% CAMERA 1 ( Simulation Data )
+CAM1_Sensor_Simulated_Data.px = CAM1_PX; 
+CAM1_Sensor_Simulated_Data.py = CAM1_PY; 
+CAM1_Sensor_Simulated_Data.vx = CAM1_VX;
+CAM1_Sensor_Simulated_Data.vy = CAM1_VY;
+CAM1_Sensor_Simulated_Data.MeasNoiseCov = CAM1_MeasNoise; 
+CAM1_Sensor_Simulated_Data.objClassID = CAM1_objClassID;
+CAM1_Sensor_Simulated_Data.detTimeStamp = CAM1_detTimeStamp;
+% ==============================================================================================================================================================
+% CAMERA 2 ( Simulation Data )
+CAM2_Sensor_Simulated_Data.px = CAM2_PX; 
+CAM2_Sensor_Simulated_Data.py = CAM2_PY; 
+CAM2_Sensor_Simulated_Data.vx = CAM2_VX;
+CAM2_Sensor_Simulated_Data.vy = CAM2_VY;
+CAM2_Sensor_Simulated_Data.MeasNoiseCov = CAM2_MeasNoise; 
+CAM2_Sensor_Simulated_Data.objClassID = CAM2_objClassID;
+CAM2_Sensor_Simulated_Data.detTimeStamp = CAM2_detTimeStamp;
+% ==============================================================================================================================================================
+% CAMERA 3 ( Simulation Data )
+CAM3_Sensor_Simulated_Data.px = CAM3_PX; 
+CAM3_Sensor_Simulated_Data.py = CAM3_PY; 
+CAM3_Sensor_Simulated_Data.vx = CAM3_VX;
+CAM3_Sensor_Simulated_Data.vy = CAM3_VY;
+CAM3_Sensor_Simulated_Data.MeasNoiseCov = CAM3_MeasNoise; 
+CAM3_Sensor_Simulated_Data.objClassID = CAM3_objClassID;
+CAM3_Sensor_Simulated_Data.detTimeStamp = CAM3_detTimeStamp;
+% ==============================================================================================================================================================
+% CAMERA 4 ( Simulation Data )
+CAM4_Sensor_Simulated_Data.px = CAM4_PX; 
+CAM4_Sensor_Simulated_Data.py = CAM4_PY; 
+CAM4_Sensor_Simulated_Data.vx = CAM4_VX;
+CAM4_Sensor_Simulated_Data.vy = CAM4_VY;
+CAM4_Sensor_Simulated_Data.MeasNoiseCov = CAM4_MeasNoise; 
+CAM4_Sensor_Simulated_Data.objClassID = CAM4_objClassID;
+CAM4_Sensor_Simulated_Data.detTimeStamp = CAM4_detTimeStamp;
+% ==============================================================================================================================================================
+% CAMERA 5 ( Simulation Data )
+CAM5_Sensor_Simulated_Data.px = CAM5_PX; 
+CAM5_Sensor_Simulated_Data.py = CAM5_PY; 
+CAM5_Sensor_Simulated_Data.vx = CAM5_VX;
+CAM5_Sensor_Simulated_Data.vy = CAM5_VY;
+CAM5_Sensor_Simulated_Data.MeasNoiseCov = CAM5_MeasNoise; 
+CAM5_Sensor_Simulated_Data.objClassID = CAM5_objClassID;
+CAM5_Sensor_Simulated_Data.detTimeStamp = CAM5_detTimeStamp;
+% ==============================================================================================================================================================
+% CAMERA 6 ( Simulation Data )
+CAM6_Sensor_Simulated_Data.px = CAM6_PX; 
+CAM6_Sensor_Simulated_Data.py = CAM6_PY; 
+CAM6_Sensor_Simulated_Data.vx = CAM6_VX;
+CAM6_Sensor_Simulated_Data.vy = CAM6_VY;
+CAM6_Sensor_Simulated_Data.MeasNoiseCov = CAM6_MeasNoise; 
+CAM6_Sensor_Simulated_Data.objClassID = CAM6_objClassID;
+CAM6_Sensor_Simulated_Data.detTimeStamp = CAM6_detTimeStamp;
+% ==============================================================================================================================================================
+% CAMERA 7 ( Simulation Data )
+CAM7_Sensor_Simulated_Data.px = CAM7_PX; 
+CAM7_Sensor_Simulated_Data.py = CAM7_PY; 
+CAM7_Sensor_Simulated_Data.vx = CAM7_VX;
+CAM7_Sensor_Simulated_Data.vy = CAM7_VY;
+CAM7_Sensor_Simulated_Data.MeasNoiseCov = CAM7_MeasNoise; 
+CAM7_Sensor_Simulated_Data.objClassID = CAM7_objClassID;
+CAM7_Sensor_Simulated_Data.detTimeStamp = CAM7_detTimeStamp;
+% ==============================================================================================================================================================
+% CAMERA 8 ( Simulation Data )
+CAM8_Sensor_Simulated_Data.px = CAM8_PX; 
+CAM8_Sensor_Simulated_Data.py = CAM8_PY; 
+CAM8_Sensor_Simulated_Data.vx = CAM8_VX;
+CAM8_Sensor_Simulated_Data.vy = CAM8_VY;
+CAM8_Sensor_Simulated_Data.MeasNoiseCov = CAM8_MeasNoise; 
+CAM8_Sensor_Simulated_Data.objClassID = CAM8_objClassID;
+CAM8_Sensor_Simulated_Data.detTimeStamp = CAM8_detTimeStamp;
+% ===============================================================> END OF SCRIPT <===================================================================
